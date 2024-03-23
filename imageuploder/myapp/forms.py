a6b2e7646193e9b478d2b46762bc3c444e691b65
prajapatankit8875 <ankitkumawat027@gmail.com>
2024-03-22 21:31:15 -0400

    project done

--- imageuploder/myapp/forms.py
+++ imageuploder/myapp/forms.py
@@ -3,4 +3,6 @@ from.models import image
 
 class imageForm(forms.ModelForm):
     class Meta:
+        model = image
         fields = '__all__'
+        labels = {'photo':''}
--- imageuploder/myapp/views.py
+++ imageuploder/myapp/views.py
@@ -4,5 +4,10 @@ from.models import image
 
 # Create your views here.
 def home(request):
+    if request.method == "post"
+    form = imageForm(request.post,request.files)
+    if form.is_valid():
+      form.save()
     form = imageForm()
+    img = image.objects.all()
     return render(request, 'myapp/home.html', {'form':form})
